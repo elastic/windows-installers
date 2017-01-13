d6279d0021d1178061f7d078e043dd1b9fa3ffb3
Mikkel Nylander Bundgaard <mikkelbu@gmail.com>
2017-01-13 21:16:54 +0100

    Install wix binaries as part of the build
    This commit makes installing the wix toolset sdk no longer required.
    
    * Changes done automatically by "paket install"
    
    * Add new dependency and use existing dependency
    
    Add dependency to WixSharp.wix.bin and use existing
    dependency Microsoft.Deployment.WindowsInstaller.
    
    Fixes #1
    
    * Reverted requirement on WiX to build from source
    
    Relates #1

--- src/Elastic.Installer.Msi/Program.cs
+++ src/Elastic.Installer.Msi/Program.cs
@@ -130,6 +130,10 @@ namespace Elastic.Installer.Msi
 			project.WixSourceGenerated += PatchWixSource;
 			project.IncludeWixExtension(WixExtension.NetFx);
 
+			const string wixLocation = @"..\..\packages\WixSharp.wix.bin\tools\bin";
+			if (!Directory.Exists(wixLocation))
+			  throw new Exception($"The directory '{wixLocation}' could not be found");
+			Compiler.WixLocation = wixLocation;
 			Compiler.BuildWxs(project, $@"_Generated\{productName.ToLower()}.wxs", Compiler.OutputType.MSI);
 			Compiler.BuildMsi(project);
 		}
