531a8bed640e69de2b51c08d4d681d28d0fafc5a
Russ Cam <russ.cam@elastic.co>
2017-05-31 14:33:14 +1000

    Fix integration tests
    Missing $Version passed to msiexec install/uninstall

--- src/Tests/Elastic.Installer.Integration.Tests/Common/PesterBootstrap.ps1
+++ src/Tests/Elastic.Installer.Integration.Tests/Common/PesterBootstrap.ps1
@@ -1,19 +1,35 @@
+<#
+.Synopsis
+    Bootstraps Pester on a Vagrant box and invokes the Tests in the $PWD.Drive.Root\vagrant directory
+.Example
+	.\PesterBootstrap.ps1 -Version "5.4.0" -TestDirName "Silent-Install"
+.Parameter TestDirName
+    The name of the test directory containing the tests
+.Parameter Version
+	The product version under test
+#>
 [CmdletBinding()]
 Param(
     [Parameter(Mandatory=$true)]
     [string] $TestDirName,
 
     [Parameter(Mandatory=$true)]
+	[ValidatePattern("\d+\.\d+\.\d+((?:\-[\w\-]+))?")]
     [string] $Version
 )
-
-$currentDir = Split-Path -parent $MyInvocation.MyCommand.Path
-Set-Location $currentDir
 
 # Used in tests
 $env:EsVersion = $Version
 
+$currentDir = Split-Path -parent $MyInvocation.MyCommand.Path
+cd $currentDir
+
+$drive = $PWD.Drive.Root
 $pester = "Pester"
+$date = Get-Date -format "yyyy-MM-ddT-HHmmssfff"
+$path = "$($drive)out\results-$TestDirName-$Version-$date.xml"
+
+# Load the Pester module into the current session. Install if not available
 Write-Output "import $pester"
 if(-not(Get-Module -Name $pester)) 
 { 
@@ -26,8 +42,4 @@ if(-not(Get-Module -Name $pester))
 	}
 }
 
-
-$Date = Get-Date -format "yyyy-MM-ddT-HHmmssfff"
-$Path = ".\..\out\results-$TestDirName-$Version-$Date.xml"
-
-Invoke-Pester -Path '.\..\vagrant\*' -OutputFile $Path -OutputFormat "NUnitXml"
+Invoke-Pester -Path '$($drive)vagrant\*' -OutputFile $path -OutputFormat "NUnitXml"
