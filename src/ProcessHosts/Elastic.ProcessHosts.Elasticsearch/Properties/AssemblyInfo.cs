ce921d2fc9fbece871e20dbaabd9c6774da26f7c
Russ Cam <russ.cam@elastic.co>
2017-08-11 11:39:14 +1000

    Fix integration tests
    When boolean settings are false e.g. bootstrap.memory_lock, don't expect to find them in Elasticsearch config
    When Checking the MSI version, remove any prerelease suffix as the registry for the installation will not have this
    
    (cherry picked from commit fef7f24b534d00d94059f1335a72b4af1da9c2da)

--- src/ProcessHosts/Elastic.ProcessHosts.Elasticsearch/Properties/AssemblyInfo.cs
+++ src/ProcessHosts/Elastic.ProcessHosts.Elasticsearch/Properties/AssemblyInfo.cs
@@ -6,23 +6,23 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescriptionAttribute("Elasticsearch is a distributed, RESTful search and analytics engine capable of solving a growing number of use cases. As the heart of the Elastic Stack, it centrally stores your data so you can discover the expected and uncover the unexpected.")]
 [assembly: GuidAttribute("d4fb307f-cb1d-4026-bd28-ca1d0016d709")]
 [assembly: AssemblyProductAttribute("Elasticsearch")]
-[assembly: AssemblyMetadataAttribute("GitBuildHash","44ab5e")]
+[assembly: AssemblyMetadataAttribute("GitBuildHash","d96362")]
 [assembly: AssemblyCompanyAttribute("Elasticsearch BV")]
 [assembly: AssemblyCopyrightAttribute("Apache License, version 2 (ALv2). Copyright Elasticsearch.")]
 [assembly: AssemblyTrademarkAttribute("Elasticsearch is a trademark of Elasticsearch BV, registered in the U.S. and in other countries.")]
-[assembly: AssemblyVersionAttribute("5.4.3")]
-[assembly: AssemblyFileVersionAttribute("5.4.3")]
+[assembly: AssemblyVersionAttribute("5.5.1")]
+[assembly: AssemblyFileVersionAttribute("5.5.1")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "Elasticsearch, you know for search!";
         internal const System.String AssemblyDescription = "Elasticsearch is a distributed, RESTful search and analytics engine capable of solving a growing number of use cases. As the heart of the Elastic Stack, it centrally stores your data so you can discover the expected and uncover the unexpected.";
         internal const System.String Guid = "d4fb307f-cb1d-4026-bd28-ca1d0016d709";
         internal const System.String AssemblyProduct = "Elasticsearch";
-        internal const System.String AssemblyMetadata_GitBuildHash = "44ab5e";
+        internal const System.String AssemblyMetadata_GitBuildHash = "d96362";
         internal const System.String AssemblyCompany = "Elasticsearch BV";
         internal const System.String AssemblyCopyright = "Apache License, version 2 (ALv2). Copyright Elasticsearch.";
         internal const System.String AssemblyTrademark = "Elasticsearch is a trademark of Elasticsearch BV, registered in the U.S. and in other countries.";
-        internal const System.String AssemblyVersion = "5.4.3";
-        internal const System.String AssemblyFileVersion = "5.4.3";
+        internal const System.String AssemblyVersion = "5.5.1";
+        internal const System.String AssemblyFileVersion = "5.5.1";
     }
 }
