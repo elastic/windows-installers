03cbd7c2a9eee09af8e7462af59d69cedac37df2
Russ Cam <russ.cam@elastic.co>
2017-08-23 12:27:07 +1000

    Exit early when paket operation fails
    If there is an issue with restoring packages, then exit the build process early.
    
    Fixes elastic/release-manager#158

--- build.bat
+++ build.bat
@@ -1,6 +1,14 @@
 @echo off
 
 .paket\paket.bootstrapper.exe
-IF EXIST paket.lock (.paket\paket.exe restore)
-IF NOT EXIST paket.lock (.paket\paket.exe install)
+IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+
+IF EXIST paket.lock (
+	.paket\paket.exe restore
+	IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+)
+IF NOT EXIST paket.lock (
+	.paket\paket.exe install
+	IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+)
 "packages\build\FAKE\tools\Fake.exe" "build\\scripts\\Targets.fsx" "cmdline=%*"
