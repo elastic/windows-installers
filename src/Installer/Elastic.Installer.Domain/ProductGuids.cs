d090ad2733cb0544a3016f0e1b712dc1b47653b7
Russ Cam <russ.cam@forloop.co.uk>
2017-09-22 23:49:13 +1000

    Update 6.0.0-rc1 guid
    (cherry picked from commit 10cc94fed28f360ea3d6aa0f2f3bd81c71ba87f9)

--- src/Installer/Elastic.Installer.Domain/ProductGuids.cs
+++ src/Installer/Elastic.Installer.Domain/ProductGuids.cs
@@ -40,7 +40,7 @@ namespace Elastic.Installer.Domain
 			{ "5.6.1", new Guid("5e6f4d7a-9220-4432-9e5f-c9a9ab184dea") },
 			{ "6.0.0-beta1", new Guid("f234f31d-f25c-4e64-8d3e-868f7b7ece97") },
 			{ "6.0.0-beta2", new Guid("50f217e3-97b7-47ba-95f7-685f7db0c8aa") },
-			{ "6.0.0-rc1", new Guid("9f373edb-d845-435d-8705-76e9e2a11e6a") },
+			{ "6.0.0-rc1", new Guid("23c31d4d-6990-4c2e-92c5-4dd3348d3e07") },
 			{ "6.0.0", new Guid("e962ab93-6e24-4128-bacb-93422cdcd50d") },
 			{ "6.1.0", new Guid("7280a702-8532-4341-ac9e-38b221d14c1c") },
 			{ "7.0.0-alpha1", new Guid("303b8268-6caa-4b82-aa3a-2b3e7197a0a3") }
