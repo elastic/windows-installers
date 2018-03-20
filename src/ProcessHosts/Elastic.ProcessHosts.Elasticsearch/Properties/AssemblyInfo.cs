0370e7b00ed11e5cf9681e9ee944fec4dc6660c4
Mpdreamz <mpdreamz@gmail.com>
2018-03-20 21:13:07 +0100

    Long file names introduced more duplicates
    Some files in machine learning live in <framework>\lib\long-file.ext
    
    The ids we generate for duplicates prefix the parent folder to dedup,
    this has worked in the past but since these are abbreviated by default
    they all ended up as lib._...ile.ext.
    
    We now dedup by taking the whole path from the root into account.
    
    Bump to build 6.3.0 (currently a fake one baked of the merged xpack
    master)

--- src/ProcessHosts/Elastic.ProcessHosts.Elasticsearch/Properties/AssemblyInfo.cs
+++ src/ProcessHosts/Elastic.ProcessHosts.Elasticsearch/Properties/AssemblyInfo.cs
@@ -6,25 +6,25 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescriptionAttribute("Elasticsearch is a distributed, RESTful search and analytics engine capable of solving a growing number of use cases. As the heart of the Elastic Stack, it centrally stores your data so you can discover the expected and uncover the unexpected.")]
 [assembly: GuidAttribute("d4fb307f-cb1d-4026-bd28-ca1d0016d709")]
 [assembly: AssemblyProductAttribute("Elasticsearch")]
-[assembly: AssemblyMetadataAttribute("GitBuildHash","2fa2c1")]
+[assembly: AssemblyMetadataAttribute("GitBuildHash","f2e5a2")]
 [assembly: AssemblyCompanyAttribute("Elasticsearch BV")]
 [assembly: AssemblyCopyrightAttribute("Apache License, version 2 (ALv2). Copyright Elasticsearch.")]
 [assembly: AssemblyTrademarkAttribute("Elasticsearch is a trademark of Elasticsearch BV, registered in the U.S. and in other countries.")]
-[assembly: AssemblyVersionAttribute("6.1.2")]
-[assembly: AssemblyFileVersionAttribute("6.1.2")]
-[assembly: AssemblyInformationalVersionAttribute("6.1.2")]
+[assembly: AssemblyVersionAttribute("6.3.0")]
+[assembly: AssemblyFileVersionAttribute("6.3.0")]
+[assembly: AssemblyInformationalVersionAttribute("6.3.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "Elasticsearch, you know for search!";
         internal const System.String AssemblyDescription = "Elasticsearch is a distributed, RESTful search and analytics engine capable of solving a growing number of use cases. As the heart of the Elastic Stack, it centrally stores your data so you can discover the expected and uncover the unexpected.";
         internal const System.String Guid = "d4fb307f-cb1d-4026-bd28-ca1d0016d709";
         internal const System.String AssemblyProduct = "Elasticsearch";
-        internal const System.String AssemblyMetadata_GitBuildHash = "2fa2c1";
+        internal const System.String AssemblyMetadata_GitBuildHash = "f2e5a2";
         internal const System.String AssemblyCompany = "Elasticsearch BV";
         internal const System.String AssemblyCopyright = "Apache License, version 2 (ALv2). Copyright Elasticsearch.";
         internal const System.String AssemblyTrademark = "Elasticsearch is a trademark of Elasticsearch BV, registered in the U.S. and in other countries.";
-        internal const System.String AssemblyVersion = "6.1.2";
-        internal const System.String AssemblyFileVersion = "6.1.2";
-        internal const System.String AssemblyInformationalVersion = "6.1.2";
+        internal const System.String AssemblyVersion = "6.3.0";
+        internal const System.String AssemblyFileVersion = "6.3.0";
+        internal const System.String AssemblyInformationalVersion = "6.3.0";
     }
 }
