8358978998989a5027247deae32667b83193183c
Yuriy <51912343+ygel@users.noreply.github.com>
2021-02-11 12:18:09 -0600

    Backport 7.11: JvmOptionsParser requires a path to the plugins directory as well.
    ref #401
    ref #402
    
    master: https://github.com/elastic/windows-installers/commit/f53ace7477792ab4d833cf9740501e38e580b410

--- src/ProcessHosts/Elastic.ProcessHosts/Elasticsearch/Process/ElasticsearchProcess.cs
+++ src/ProcessHosts/Elastic.ProcessHosts/Elasticsearch/Process/ElasticsearchProcess.cs
@@ -124,9 +124,21 @@ namespace Elastic.ProcessHosts.Elasticsearch.Process
 			
 			if (!this.JavaVersionChecker.Start(null, out var javaVersionOut))
 				throw new StartupException($"Invalid Java version reported: {javaVersionOut}");
-			
-			if (!this.JvmOptionsParser.Start(new [] { $"\"{this.ConfigDirectory}\"" }, out var jvmOptionsLine) || string.IsNullOrWhiteSpace(jvmOptionsLine)) 
+
+			var pluginsDir = Path.Combine(this.HomeDirectory, "plugins");
+
+			if (!Directory.Exists(pluginsDir))
+				Directory.CreateDirectory(pluginsDir);
+
+			var requiredArgs = new[]
+			{
+				$"\"{this.ConfigDirectory}\"",
+				$"\"{pluginsDir}\"",
+			};
+
+			if (!this.JvmOptionsParser.Start(requiredArgs, out var jvmOptionsLine) || string.IsNullOrWhiteSpace(jvmOptionsLine))
 				throw new StartupException($"Could not evaluate jvm.options file. result: {jvmOptionsLine}");
+
 			jvmOptionsLine = jvmOptionsLine.Replace("${ES_TMPDIR}", this.PrivateTempDirectory).Replace("\n", "").Replace("\r", "");
 
 			return new []
